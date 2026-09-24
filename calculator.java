307b44971eaa5f616b20f73dad9c968839699cd9
Simeon Hristov <simeon.hristov@yahoo.com>
2026-09-24 16:13:07 +0300

    added

--- calculator.java
+++ calculator.java
@@ -3,15 +3,15 @@ public class Calculator {
     public double add(double a, double b) {
         return a + b;
     }
-
+    // addition
     public double subtract(double a, double b) {
         return a - b;
     }
-
+    // subtraction
     public double multiply(double a, double b) {
         return a * b;
     }
-    
+    // multiplication
     public double divide(double a, double b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero.");
